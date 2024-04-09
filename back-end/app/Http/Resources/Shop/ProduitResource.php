3cb71affb93b0d06f11e14ef8888f7af34c44741
KpihX <kapoivha@gmail.com>
2024-04-09 21:03:29 +0100

    adding of basic functionnalities

--- back-end/app/Http/Resources/Shop/ProduitResource.php
+++ back-end/app/Http/Resources/Shop/ProduitResource.php
@@ -1,19 +1,48 @@
 <?php
 
+// namespace App\Http\Resources;
+
+// use Illuminate\Http\Request;
+// use Illuminate\Http\Resources\Json\JsonResource;
+
+// class ProduitResource extends JsonResource
+// {
+//     /**
+//      * Transform the resource into an array.
+//      *
+//      * @return array<string, mixed>
+//      */
+//     public function toArray(Request $request): array
+//     {
+//         return parent::toArray($request);
+//     }
+// }
+
 namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProduitResource extends JsonResource
 {
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
+    public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'codePro' => $this->codePro,
+            'idCategorie' => $this->idCategorie,
+            'nomPro' => $this->nomPro,
+            'prix' => $this->prix,
+            'qte' => $this->qte,
+            'description' => $this->description,
+            'codeArrivage' => $this->codeArrivage,
+            'actif' => $this->actif,
+            'dateInsertion' => $this->dateInsertion,
+            'prixAchat' => $this->prixAchat,
+            'pourcentage' => $this->pourcentage,
+            'promo' => $this->promo,
+            'size1' => $this->size1,
+            'size2' => $this->size2,
+            'typeSize' => $this->typeSize,
+            'images' => PhotoResource::collection($this->photos), // Assuming a relationship 'photos' exists
+        ];
     }
 }
